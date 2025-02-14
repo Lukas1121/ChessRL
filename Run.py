0dc99cc201f9d612d81fafd6d7f1a07692483f61
Lukas1121 <lukas.zeppelin.ry@gmail.com>
2025-02-14 12:44:29 +0100

    version issues

--- Run.py
+++ Run.py
@@ -1,19 +1,22 @@
-import Helper_functions as hf
+import Helper_functions as functions
 
-metrics = hf.train_chess_policy_networks(
-    num_iterations=1000,
-    games_per_iteration=10,
+
+
+metrics = functions.train_chess_policy_networks(
+    num_iterations=2000,
+    games_per_iteration=5,
     epsilon_initial=0.1,
     epsilon_final=0.0,
     lr=0.00001,
     gamma=0.95,
     terminal_reward=1000,
     terminal_loss=100,
-    per_move_penalty=1,
+    per_move_penalty=0,
     non_capture_penalty=-0.15,
     move_length_threshold=120,
-    exceeding_length_penalty=100,
-    pretrained_model_path_white="run0/policy_net_white.pth",
-    pretrained_model_path_black="run0/policy_net_black.pth",)
+    exceeding_length_penalty=0,
+    pretrained_model_path_white=None,
+    pretrained_model_path_black=None,
+)
 
-hf.plot_training_metrics_binned(metrics)
+functions.plot_training_metrics_binned(metrics)
